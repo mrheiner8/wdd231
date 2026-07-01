f7df1c9c050b3f65a5499d7d0497dc1820c125be
mrheiner8 <mrheiner8@gmail.com>
2026-06-30 22:34:50 -0600

    done for the night

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,16 +2,6 @@
 const today = new Date();
 const currentYear = today.getFullYear();
 
-// Set constants
-const navButton = document.querySelector('#nav-button'); 
-
-// nav button
-navButton.addEventListener('click', () => {
-    navButton.classList.toggle('show');
-});
-
-
-
 // Inject the footer dates into the HTML spans
 document.getElementById("currentYear").innerHTML = currentYear;
 document.getElementById("lastModified").innerHTML = document.lastModified;
