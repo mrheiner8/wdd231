66a55dbc41b07a63d5816acd017aa88dd44f1591
mrheiner8 <mrheiner8@gmail.com>
2026-07-07 21:17:31 -0500

    json

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,44 @@
+const url = 'https://mrheiner8.github.io/wdd231/chamber/data/members.json';
+const membersContainer = document.querySelector('#members');
+
+async function getMembersData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayMembers(data.members);
+}
+getMembersData();
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement("section");
+        let companyName = document.createElement("h2");
+        let location = document.createElement("p");
+        let pNumber = document.createElement("p");
+        let years = document.createElement("p");
+        let site = document.createElement("a");
+        let picture = document.createElement("img");
+    
+        companyName.textContent = `${member.company}`;
+        years.textContent = `${member.years} in business`;
+        location.textContent = `Address: ${member.address}`;
+        pNumber.textContent = `Phone Number: ${member.phone}`;
+        site.textContent = `${member.url}`;
+
+        site.setAttribute('href', member.url);
+
+        picture.setAttribute('src', member.imagefile);
+        picture.setAttribute('alt', `Picture of ${member.company}`);
+        picture.setAttribute('loading', 'lazy');
+        picture.setAttribute('width', '340');
+        picture.setAttribute('height', '440');
+
+        card.appendChild(companyName);
+        card.appendChild(years);
+        card.appendChild(location);
+        card.appendChild(pNumber);
+        card.appendChild(site);
+        card.appendChild(picture);
+
+        membersContainer.appendChild(card);
+    });
+}
